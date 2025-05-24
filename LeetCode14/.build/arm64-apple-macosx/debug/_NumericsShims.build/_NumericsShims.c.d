61a022a89bf24e2dd160a1aca934186f774cf6bc
Shani <106402127+ShaniDan@users.noreply.github.com>
2025-05-24 16:03:30 -0700

    initial commit

---
+++ LeetCode14/.build/arm64-apple-macosx/debug/_NumericsShims.build/_NumericsShims.c.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/shakhnozamirabzalova/Developer/Problem-Solving2/LeetCode14/.build/checkouts/swift-numerics/Sources/_NumericsShims/include/module.modulemap \
+  /Users/shakhnozamirabzalova/Developer/Problem-Solving2/LeetCode14/.build/checkouts/swift-numerics/Sources/_NumericsShims/_NumericsShims.c \
+  /Users/shakhnozamirabzalova/Developer/Problem-Solving2/LeetCode14/.build/checkouts/swift-numerics/Sources/_NumericsShims/include/_NumericsShims.h
