6ebbdefd494b37a6c845cab6b999e864d1dfc897
Shani <106402127+ShaniDan@users.noreply.github.com>
2025-09-26 11:29:28 -0700

    P01

---
+++ Interview-Prep.playground/Pages/P01_LL.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
+
+// Find the last element of a linked list.
+
+class List<T> {
+    var value: T
+    var next: List<T>?
+    
+    init(value: T, next: List<T>? = nil) {
+        self.value = value
+        self.next = next
+    }
+}
+
+
+extension List {
+    var last: T? {
+        
+    }
+    return nil
+}
