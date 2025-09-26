4a3e6c21141239d44f3f3d6f97eca36b90f743b3
Shani <106402127+ShaniDan@users.noreply.github.com>
2025-09-26 11:57:18 -0700

    P01

--- Interview-Prep.playground/Pages/P01_LL.xcplaygroundpage/Contents.swift
+++ Interview-Prep.playground/Pages/P01_LL.xcplaygroundpage/Contents.swift
@@ -6,8 +6,9 @@ var greeting = "Hello, playground"
 
 //: [Next](@next)
 
-// Find the last element of a linked list.
+// MARK: Find the last element of a linked list.
 
+// Define the linked list class, <T>  means it can be type of any
 class List<T> {
     var value: T
     var next: List<T>?
@@ -21,7 +22,32 @@ class List<T> {
 
 extension List {
     var last: T? {
-        
+        if let next = next {
+            return next.last
+        } else {
+            return value
+        }
     }
-    return nil
 }
+
+// Create nodes
+let node1 = List(value: 1)
+let node2 = List(value: 1)
+let node3 = List(value: 2)
+let node4 = List(value: 3)
+let node5 = List(value: 5)
+let node6 = List(value: 8)
+
+// Link nodes, that's how the nodes are linked
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+node5.next = node6
+
+// Get the last element
+print(node1.last!)
+
+
+
+
