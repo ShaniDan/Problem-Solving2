ff6aca78051cdb21e586df48aaa3e25122f2ea6e
Shani <106402127+ShaniDan@users.noreply.github.com>
2025-10-14 10:26:37 -0700

    P07

--- Interview-Prep.playground/Pages/P07_LL.xcplaygroundpage/Contents.swift
+++ Interview-Prep.playground/Pages/P07_LL.xcplaygroundpage/Contents.swift
@@ -26,14 +26,24 @@ class List<T> {
     var value: T
     var next: List<T>?
     
-    init(value: T, next: List<T>?) {
+    init(value: T, next: List<T>? = nil) {
         self.value = value
         self.next = next
     }
 }
 
 extension List {
+    
     func flatten() {
-        
+        print(#function, "current node: \(value)")
     }
 }
+
+// create nodes
+let node1 = List(value: 1)
+let node2 = List(value: 2)
+let node3 = List(value: 3)
+
+node1.next = node2
+node2.next = node3
+
