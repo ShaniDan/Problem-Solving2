6e0667d09d6ba29a41a7c8d0f75fbb2d2693769f
Shani <106402127+ShaniDan@users.noreply.github.com>
2026-01-07 14:43:56 -0800

    changes count apples and oranges to O(1)

--- SwiftAlgos.playground/Pages/HR-CountApplesAndOranges.xcplaygroundpage/Contents.swift
+++ SwiftAlgos.playground/Pages/HR-CountApplesAndOranges.xcplaygroundpage/Contents.swift
@@ -11,6 +11,9 @@ var greeting = "Hello, playground"
 
 // MARK: This isn't passing big number cases
 
+// This is memory heavy because I'm storing the results in new arrays
+//O(n) - Linear time - good for small inputs
+
 func countApplesAndOranges(s: Int, t: Int, a: Int, b: Int, apples: [Int], oranges: [Int]) -> Void {
     var sumApples = [Int]()
     var sumOranges = [Int]()
@@ -46,3 +49,28 @@ func countApplesAndOranges(s: Int, t: Int, a: Int, b: Int, apples: [Int], orange
     print(orangeCount)
 }
 countApplesAndOranges(s: 7, t: 10, a: 4, b: 12, apples: [2,3,-4], oranges: [3,-2,-4])
+
+//O(1) - Constant time is faster and more scalable than O(n) - good for large inputs
+func countApplesAndOranges2(s: Int, t: Int, a: Int, b: Int, apples: [Int], oranges: [Int]) -> Void {
+    var appleCount2 = 0
+    var orangeCount2 = 0
+    
+    for number in apples {
+        let landingPosition = a + number
+        if landingPosition >= s && landingPosition <= t {
+            appleCount2 += 1
+        }
+    }
+    
+    for number in oranges {
+        let landingPosition = b + number
+        
+        if landingPosition >= s && landingPosition <= t {
+            orangeCount2 += 1
+        }
+    }
+    print(appleCount2)
+    print(orangeCount2)
+}
+
+countApplesAndOranges2(s: 7, t: 10, a: 4, b: 12, apples: [2,3,-4], oranges: [3,-2,-4])
