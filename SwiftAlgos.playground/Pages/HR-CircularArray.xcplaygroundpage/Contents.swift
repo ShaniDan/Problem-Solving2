502dd0907c5d9deb985046ab6074fa600d5938e1
Shani <106402127+ShaniDan@users.noreply.github.com>
2026-02-02 17:46:43 -0800

    hr circular array

--- SwiftAlgos.playground/Pages/HR-CircularArray.xcplaygroundpage/Contents.swift
+++ SwiftAlgos.playground/Pages/HR-CircularArray.xcplaygroundpage/Contents.swift
@@ -6,10 +6,37 @@ var greeting = "Hello, playground"
 
 //: [Next](@next)
 func circularArrayRotation(a: [Int], k: Int, queries: [Int]) -> [Int] {
-    var a2 = a
     var result = [Int]()
-    a2.swapAt(0, a2.count - 1)
+    var a2 = a
+    
+    for _ in 1...k {
+        let changedArray: Optional<Int> = a2.popLast()
+        if let unwrappedChangedArray = changedArray {
+            a2.insert(unwrappedChangedArray, at: 0)
+        }
+    }
+//    switch changedArray {
+//    case .some(let int):
+//        print("changedArray was \(int)")
+//    case .none:
+//        print("changedArray was nil")
+//    }
+    
+    for index in queries {
+        result.append(a2[index])
+    }
     print(a2)
     return result
 }
 print(circularArrayRotation(a: [3, 4, 5], k: 2, queries: [1,2]))
+
+
+//Recursive
+
+func circularArrayRotation2(a: [Int], k: Int, queries: [Int]) -> [Int] {
+    var result = [Int]()
+    
+    return result
+    
+}
+print(circularArrayRotation2(a: [3, 4, 5], k: 2, queries: [1,2]))
