c25c3723f65d57ecc583cb5cd4b3e0375d4e58f2
Shani <106402127+ShaniDan@users.noreply.github.com>
2026-05-01 12:09:56 -0700

    hr bonAppetit

---
+++ SwiftAlgos.playground/Pages/HR-BonAppetit.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+// TODO: Need to work on this challenge
+// https://www.hackerrank.com/challenges/bon-appetit/problem?isFullScreen=true
+
+func bonAppetit(bill: [Int], k: Int, b: Int) -> Void {
+    var mutBill = bill
+    var newBill = [Int]()
+    for num in mutBill.indices {
+        if mutBill[num] == mutBill[k] {
+            mutBill.remove(at: num)
+            print(num)
+            newBill = mutBill
+        }
+    }
+    let sum = newBill.reduce(0, +)
+    if newBill.reduce(0, +) % 2 == 0  {
+       print("Bon Appetit")
+    } else {
+        print(b)
+    }
+    
+    
+}
+print(bonAppetit(bill: [3, 10, 2, 9], k: 1, b: 12))
