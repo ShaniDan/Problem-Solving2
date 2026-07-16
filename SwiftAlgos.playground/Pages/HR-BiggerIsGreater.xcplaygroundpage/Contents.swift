10362485cd3d4afc5cd41a540244f40b762f2a09
Shani <106402127+ShaniDan@users.noreply.github.com>
2026-07-16 08:20:29 -0700

    String indices

--- SwiftAlgos.playground/Pages/HR-BiggerIsGreater.xcplaygroundpage/Contents.swift
+++ SwiftAlgos.playground/Pages/HR-BiggerIsGreater.xcplaygroundpage/Contents.swift
@@ -2,18 +2,32 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
-
 //: [Next](@next)
+
+let greeting = "Hello, world!"
+let index = greeting.firstIndex(of: ",") ?? greeting.endIndex
+print(index)
+let beginning = greeting[..<index]
+print(beginning)
+let newString = String(beginning)
+print(newString)
+
+let quotation = "We're a lot alike, you and I."
+let sameQuotation = "We're a lot alike, you and I."
+if quotation == sameQuotation {
+    print("These two strings are considered equal")
+}
+
 func biggerIsGreater(w: String) -> String {
     // word is greater than the original word
     // the smallest word that meets the first condition
     // swap the last letter
+    
     print(w.endIndex)
+    
     for index in w.indices {
         print(w[index])
     }
     return ""
-    
 }
 print(biggerIsGreater(w: "abcd"))
