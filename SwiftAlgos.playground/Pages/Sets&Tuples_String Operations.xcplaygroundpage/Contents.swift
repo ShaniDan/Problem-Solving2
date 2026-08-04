85dbc92841fa209b937694d68e6eb79b8c5617fb
Shani <106402127+ShaniDan@users.noreply.github.com>
2026-08-04 13:47:36 -0700

    String Operations

--- SwiftAlgos.playground/Pages/Sets&Tuples_String Operations.xcplaygroundpage/Contents.swift
+++ SwiftAlgos.playground/Pages/Sets&Tuples_String Operations.xcplaygroundpage/Contents.swift
@@ -15,3 +15,18 @@ print(colors.count)
 // tuple
 let person = (name: "Josh", age: 28)
 print("\(person.name) is \(person.age)")
+
+/*
+ Create a string "Swift,UIKit,SwiftUI". Split it by comma into an array. Join the array with " & " and print the result: "Swift & UIKit & SwiftUI".
+ */
+
+let string = "Swift,UIKit,SwiftUI"
+// split changes the String type into a [Substring]
+let split = string.split(separator: ",")
+print(split)
+let stringArray = split.map(String.init)
+print(stringArray)
+let joined = split.joined(separator: " & ")
+print(joined)
+
+
